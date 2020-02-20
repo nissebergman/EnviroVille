8eae8d14d0b8d20eedf9e74250d839badcafd263
nissebergman <nibe91@gmail.com>
2020-02-20 16:46:22 +0100

    Lagade heroku

--- WebGL/index.php
+++ WebGL/index.php
@@ -1,18 +1,1 @@
-<!DOCTYPE HTML>
-<html>
-
-<head>
-    <meta name="viewport" content="width=device-width, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0">
-    <script src='JS/three.min.js'></script>
-    <script src='JS/OrbitControls.js'></script>
-    <script src='JS/GLTFLoader.js'></script>
-    <script src='Extern/noisejs/perlin.js'></script>
-    <link rel="stylesheet" href="Style/style.css">
-</head>
-
-<body>
-
-    <script src="JS/main.js"></script>
-</body>
-
-</html>
+<?php include="index.html" ?>
